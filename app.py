93884118cce38279058d13f30c7e28d9597faad3
haithem ben mansour <69584299+haithembm@users.noreply.github.com>
2026-03-14 15:01:24 +0000

    more

--- app.py
+++ app.py
@@ -2,10 +2,34 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+Jobs = [
+    {'id' : 1,
+        'title' :'Data Analyst',
+         'location' : 'Bengaluru, India',
+         'salary' : 'Rs. 1000000',
+           },
+    {'id' : 2,
+        'title' :'Data Scientist',
+         'location' : 'Delhi, India'
+           },
+    {'id' : 3,
+        'title' :'Frontend Engineer',
+         'location' : 'Remote',
+         'salary' : 'Rs. 1300000',
+           },
+    {'id' : 4,
+        'title' :'Backtend Engineer',
+         'location' : 'San Fran',
+         'salary' : '$ 130',
+           }
+
+]
+
 @app.route("/")
 
 def Hello_world():
-    return render_template("home.html")
+    return render_template("home.html", jobs=Jobs,
+    company_name=   "Jovian" )
 
 if __name__ == "__main__":  
     # on running python app.py    
